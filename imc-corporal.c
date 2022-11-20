4c307de994032f4d341881bca127e93a01966acf
devMRNGN <joao.tadeuzi00@gmail.com>
2022-11-20 13:26:56 -0300

    alterando nomes

--- imc-corporal.c
+++ imc-corporal.c
@@ -7,11 +7,11 @@ printf("\n============VEJA SE VOCE ESTA NO PESO IDEAL===============");
 printf("\nQual o peso da pessoa?");
 scanf("%f",&peso);
 printf("\nQual a altura da pessoa?");
-scanf("f",&altura);
+scanf("%f",&altura);
 relacao = peso / (altura*altura);
 if(relacao < 20){
     printf("\n=========VOCE ESTA ABAIXO DO PESO==========");
-}else if(20 >= relacao && relacao < 25){
+}else if(relacao >= 20 && relacao < 25){
     printf("\n===========VOCE ESTA NO SEU PESO IDEAL, PARABENS===========");
 }else if(relacao >= 25){
     printf("\n===============VOCE ESTA ACIMA DO PESO===============");
